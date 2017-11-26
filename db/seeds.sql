831f92f6fc43beb4ee200c41341b21762b9addc4
ykeanu <ykhernan@ucla.edu>
2017-11-26 10:07:47 -0800

    Database set-up

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheeseburger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheesburger ', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyakiburger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegeburger', 0);
